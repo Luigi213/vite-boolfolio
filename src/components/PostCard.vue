cf58347f0d42a1efa2c92aaaa0863e33d823f01f
Luigi213 <reyesgianluigi88@gmail.com>
2023-03-10 10:04:57 +0100

    some styled

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -11,8 +11,8 @@ export default {
         <img :src="project.post_image != null ? `${baseUrl}/storage/${project.post_image}` : 'https://picsum.photos/300/200'" :alt="project.id">
         <div class="card-body">
             <h5 class="card-title mb-4">{{project.titolo}}</h5>
-            <p class="card-text"><strong>Tipo: </strong><em>{{project.type != null ? project.type.name : 'Nessun tipo'}}</em></p>
-            <p class="card-text"><strong>Tech: </strong><em v-for="project in project.technologies" :key="project.id">{{ project.name_tech }},</em></p>
+            <p class="card-text"><strong>Tipo: </strong><em class="badge bg-warning">{{project.type != null ? project.type.name : 'Nessun tipo'}}</em></p>
+            <p class="card-text"><strong>Tech: </strong><em class="badge bg-primary" v-for="project in project.technologies" :key="project.id">{{ project.name_tech }},</em></p>
             <p class="card-text"><strong>Descrizione: </strong>{{project.descrizione}}</p>
         </div>
     </div>
