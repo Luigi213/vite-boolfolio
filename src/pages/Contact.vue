22ce59f8a37e14e3c3736eb086f6893e526c6aca
Luigi213 <reyesgianluigi88@gmail.com>
2023-03-13 15:07:46 +0100

    done sendMail

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,61 +1,86 @@
 <script>
+import axios from 'axios';
+import { store } from '../store.js';
 export default {
     data(){
         return {
-            
+            store,
+            name: '',
+            surname: '',
+            email: '',
+            number: '',
+            message: '',
+            errors: null,
+            loading: true
+        }
+    },
+    methods:{
+        sendMail(){
+            const data = {
+                name: this.name,
+                surname: this.surname,
+                email: this.email,
+                number: this.number,
+                message: this.message
+            };
+
+            axios.post(`${this.store.baseUrl}/api/contact`, data).then((response) =>{
+                if(!response.data.success){
+                    this.errors = response.data.errors
+                }
+                else{
+                    this.name = '',
+                    this.surname = '',
+                    this.email = '', 
+                    this.number = '',
+                    this.message = ''
+                }
+            });
         }
     }
 }
 </script>
 <template lang="">
-    <!-- <div class="row" v-if="loading">
-        <div class="col-12 d-flex justify-content-center align-items-center align">
-            <div class="spinner-border" role="status">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 text-center">
+                Sinistra
+            </div>
+            <div class="col-12 col-md-6 text-center">
+                Destra
             </div>
         </div>
-    </div> -->
-    <div>
+    </div>
+    <div class="bg-secondary w-100">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md-6 text-center">
-                    Sinistra
-                </div>
-                <div class="col-12 col-md-6 text-center">
-                    Destra
-                </div>
-            </div>
-        </div>
-        <div class="bg-secondary w-100">
-            <div class="container">
-                <div class="row">
+                <form @submit.prevent="sendMail">
                     <div class="row">
-                        <form  class="row mt-4" @sumbit.prevent="">
-                            <div class="col-12 col-md-6">
-                                <label for="nome">Nome</label>
-                                <input type="text" name="name" class="form-control" id="nome" placeholder="Inserire il nome">
-                            </div>
-                            <div class="col-12 col-md-6">
-                                <label for="cognome">Cognome</label>
-                                <input type="text" name="surname" class="form-control" id="cognome" placeholder="Inserire il cognome">
-                            </div>
-                            <div class="col-12 col-md-6">
-                                <label for="mail">Email</label>
-                                <input type="password" name="email" class="form-control" id="mail" placeholder="Inserire il email">
-                            </div>
-                            <div class="col-12 col-md-6">
-                                <label for="pass-word">Password</label>
-                                <input type="password" name="password" class="form-control" id="pass-word" placeholder="Inserire il password">
-                            </div>
-                            <div class="form-group">
-                                <label for="descrizione">Descrizione</label>
-                                <textarea name="description" id="descrizione" class="form-control" rows="2"></textarea>
-                            </div>
-                            <div class="form-group my-3">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
+                        <div class="col-12 col-md-6">
+                            <label for="nome">Nome</label>
+                            <input type="text" name="name" class="form-control" id="nome" placeholder="Inserire il nome" v-model="name">
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label for="cognome">Cognome</label>
+                            <input type="text" name="surname" class="form-control" id="cognome" placeholder="Inserire il cognome" v-model="surname">
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label for="mail">Email</label>
+                            <input type="email" name="email" class="form-control" id="mail" placeholder="Inserire il email" v-model="email">
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label for="numero">Numero</label>
+                            <input type="text" name="number" class="form-control" id="numero" placeholder="Inserire il numero" v-model="number">
+                        </div>
+                        <div class="form-group">
+                            <label for="messaggio">Messaggio</label>
+                            <textarea name="message" id="messaggio" class="form-control" rows="2" v-model="message"></textarea>
+                        </div>
+                        <div class="form-group my-3">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
